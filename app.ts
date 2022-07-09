81db24dd6f07f9f1c9f5a59063875933b0f58403
Rismy Rally <rismyrally@gmail.com>
2022-07-09 21:50:32 +0530

    working with tuples

--- app.ts
+++ app.ts
@@ -1,12 +1,19 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: 'John Doe',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'],
 };
+
+// person.role.push('admin');
+// person.role[1] = 10;
+
+// person.role = [0, 'admin', 'user'];
 
 let favouriteActivities: string[];
 favouriteActivities = ['Sports'];
