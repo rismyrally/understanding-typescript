cbba9aafa8ec52f8ba153c1e826720d988a071be
Rismy Rally <rismyrally@gmail.com>
2022-07-09 23:45:58 +0530

    the 'never' type

--- app.ts
+++ app.ts
@@ -6,3 +6,11 @@ userInput = 'John';
 if (typeof userInput === 'string') {
   userName = userInput;
 }
+
+
+function generateError(message: string, code: number): never {
+  throw { message, errorCode: code };
+  // while (true) {}
+}
+
+generateError('An error occurred!', 500);
