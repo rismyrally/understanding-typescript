6e63bb6b1049fcaea9f23061e0975f173a4c1644
Rismy Rally <rismyrally@gmail.com>
2022-08-06 20:16:16 +0530

    optional chaining

--- src/app.ts
+++ src/app.ts
@@ -35,6 +35,14 @@ function add(a: Combinable, b: Combinable) {
 const result = add('John', 'Doe');
 result.split(' ');
 
+const fetchedUserData = {
+  id: 'u1',
+  name: 'Max',
+  job: { title: 'CEO', description: 'My own company' },
+};
+
+console.log(fetchedUserData?.job?.title);
+
 // type UnknownEmployee = Employee | Admin;
 
 // function printEmployeeInformation(emp: UnknownEmployee) {
