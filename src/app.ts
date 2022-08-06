b73ba117985449b079422118da56b35bc6d04091
Rismy Rally <rismyrally@gmail.com>
2022-08-06 21:05:48 +0530

    nullish coalescing

--- src/app.ts
+++ src/app.ts
@@ -43,6 +43,12 @@ const fetchedUserData = {
 
 console.log(fetchedUserData?.job?.title);
 
+const userInput = '';
+
+const storedData = userInput ?? 'DEFAULT';
+
+console.log(storedData);
+
 // type UnknownEmployee = Employee | Admin;
 
 // function printEmployeeInformation(emp: UnknownEmployee) {
